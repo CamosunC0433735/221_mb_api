f19b2f6dda1a8845d61d257ddba004ff79c9bd79
David Abram-Krismer <C0433735@online.camosun.ca>
2023-01-31 09:39:09 -0800

    added data array

--- controllers/msg-api-controller.js
+++ controllers/msg-api-controller.js
@@ -7,4 +7,11 @@ const getAllMessages = (req, res) => {
 const addNewMessage = async (req, res) => {
     res.status(200).send('Successful API POST Request');
 };
+
+let messages = [
+    { id: 0, name: "David", messageTxt: "Hello world" },
+    { id: 1, name: "Paul", messageTxt: "Hi David" },
+    { id: 2, name: "George", messageTxt: "I love writing messages to my imaginary friends" }
+]
+
 export { getAllMessages, addNewMessage };
