9d717d86f1b64909496b647be0b77585222521a0
David Abram-Krismer <C0433735@online.camosun.ca>
2023-01-31 09:43:09 -0800

    Added retrieving array, and 400 error status code

--- controllers/msg-api-controller.js
+++ controllers/msg-api-controller.js
@@ -1,17 +1,23 @@
-// GET Request Handler
-const getAllMessages = (req, res) => {
-    res.status(200).send('Successful API GET Request');
-    
-};
-// POST Request Handler
-const addNewMessage = async (req, res) => {
-    res.status(200).send('Successful API POST Request');
-};
+
 
 let messages = [
     { id: 0, name: "David", messageTxt: "Hello world" },
     { id: 1, name: "Paul", messageTxt: "Hi David" },
     { id: 2, name: "George", messageTxt: "I love writing messages to my imaginary friends" }
 ]
+
+// GET Request Handler
+const getAllMessages = (req, res) => {
+    try {
+    res.status(200).json(messages);
+    } catch (err) {
+        res.status(400).send("Bad Request");
+    }
+    
+};
+// POST Request Handler
+const addNewMessage = async (req, res) => {
+    res.status(200).send('Successful API POST Request');
+};
 
 export { getAllMessages, addNewMessage };
