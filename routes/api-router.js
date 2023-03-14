e305f6e61122107fbb08f1a42f98a894f26f5fce
David Abram-Krismer <C0433735@online.camosun.ca>
2023-03-14 10:48:46 -0700

    protected post message request

--- routes/api-router.js
+++ routes/api-router.js
@@ -1,11 +1,12 @@
 import express from 'express';
 
 import { getAllMessages, addNewMessage } from '../controllers/msg-api-controller.js';
-import {registerNewUser} from '../controllers/user-api-controller.js';
+import { registerNewUser } from '../controllers/user-api-controller.js';
+import passport from 'passport';
 
 const router = express.Router();
 
-router.route('/messages').get(getAllMessages).post(addNewMessage);
+router.route('/messages').get(getAllMessages).post(passport.authenticate('basic', { session: false }), addNewMessage);
 router.route('/users').post(registerNewUser);
 
 export default router;
