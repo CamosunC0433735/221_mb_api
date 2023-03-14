ff5a08679b3ca628e5f7144ef72d2a8968f4c9b2
David Abram-Krismer <C0433735@online.camosun.ca>
2023-03-14 10:43:37 -0700

    Added basic auth

--- controllers/user-api-controller.js
+++ controllers/user-api-controller.js
@@ -1,18 +1,21 @@
 import mongoose from 'mongoose';
+import passport from 'passport';
+import { BasicStrategy } from 'passport-http';
+
 const userModel = mongoose.model('user')
 
 const registerNewUser = async (req, res) => {
     //res.status(200).send('Successful API New User POST Request');
 
     try {
-        if (await alreadyExists(req.body.email, req.body.username)){
+        if (await alreadyExists(req.body.email, req.body.username)) {
             res.status(403).send("Username or email already exists.")
         }
-        else{
+        else {
             let user = await userModel.create(req.body);
             res.status(201).send(user);
         }
-    } catch (err){
+    } catch (err) {
         console.log(req.body);
         res.status(400).send("Bad Request. The message in the body of the \
         Request is either missing or malformed.");
@@ -30,5 +33,30 @@ const alreadyExists = async (email, username) => (
         ]
     })
 );
+
+
+passport.use(new BasicStrategy(
+    (username, password, done) => {
+        userModel
+            .findOne({
+                '$or': [
+                    { email: username },
+                    { username: username }
+                ]
+            })
+            .exec(async (error, user) => {
+                if (error) return done(error);
+                // user wasn't found
+                if (!user) return done(null, false);
+                // user was found, see if it's a valid password
+                if (!await user.verifyPassword(password)) {
+                    return done(null,
+                        false);
+                }
+                return done(null, user);
+            });
+    }
+));
+
 
 export { registerNewUser };
