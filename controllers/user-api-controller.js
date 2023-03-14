f4262cbdc99ab22c525167a460768b2be7f802a5
David Abram-Krismer <C0433735@online.camosun.ca>
2023-03-14 10:20:43 -0700

    fixed a bug, added basic controller

---
+++ controllers/user-api-controller.js
@@ -0,0 +1,8 @@
+import mongoose from 'mongoose';
+const userModel = mongoose.model('user')
+
+const registerNewUser = async (req, res) => {
+    res.status(200).send('Successful API New User POST Request');
+    }
+
+export {registerNewUser};
--- routes/api-router.js
+++ routes/api-router.js
@@ -1,7 +1,11 @@
 import express from 'express';
+
 import { getAllMessages, addNewMessage } from '../controllers/msg-api-controller.js';
+import {registerNewUser} from '../controllers/user-api-controller.js';
+
 const router = express.Router();
 
 router.route('/messages').get(getAllMessages).post(addNewMessage);
+router.route('/users').post(registerNewUser);
 
 export default router;
