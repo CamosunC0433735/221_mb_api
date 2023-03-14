dcc6cd7ec6c36d4b0c592509e094f48bd1e966f3
David Abram-Krismer <C0433735@online.camosun.ca>
2023-03-14 10:37:32 -0700

    Added password hashing & salting

--- models/user-schema.js
+++ models/user-schema.js
@@ -1,8 +1,9 @@
 import mongoose from 'mongoose';
+import argon2 from 'argon2';
 
 
 const userSchema = new mongoose.Schema({
-    
+
     email: {
         type: String,
         required: true,
@@ -29,6 +30,20 @@ userSchema.set('toJSON', {
     versionKey: false,
     virtuals: true,
     transform: (doc, ret) => { delete ret._id; }
+});
+
+
+// Pre-Hook to Salt and Hash a password using argon2id
+userSchema.pre('save', async function () {
+    // hash and salt password
+    try {
+        const hash = await argon2.hash(this.password, {
+            type: argon2.argon2id
+        });
+        this.password = hash;
+    } catch (err) {
+        console.log('Error in hashing password' + err);
+    }
 });
 
 export default mongoose.model('user', userSchema);
