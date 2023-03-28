94555acdcb88952040c4df17644de7050d9f366e
David Abram-Krismer <C0433735@online.camosun.ca>
2023-03-28 10:58:43 -0700

    minor fix, updated .patch and .delete

--- routes/api-router.js
+++ routes/api-router.js
@@ -8,8 +8,8 @@ import passport from 'passport';
 const router = express.Router();
 
 router.route('/messages').get(getAllMessages).post(passport.authenticate('jwt', { session: false }), addNewMessage);
-router.route('/messages/:messageId').post(passport.authenticate('jwt', { session: false }), updateMessage);
-router.route('/messages/delete/:messageId').post(passport.authenticate('jwt', { session: false }), deleteMessage);
+router.route('/messages/:messageId').patch(passport.authenticate('jwt', { session: false }), updateMessage);
+router.route('/messages/delete/:messageId').delete(passport.authenticate('jwt', { session: false }), deleteMessage);
 router.route('/users').post(registerNewUser);
 router.route('/login').post(passport.authenticate('local', { session: false }), logInUser);
 
